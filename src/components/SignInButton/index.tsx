3c0da4a6d3ba3960cc9ffd2ef8c237f2f82f38e0
niltoneapontes <niltoneapontes@gmail.com>
2021-06-02 14:51:11 -0300

    feat: auth with github oauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,22 +2,26 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './style.module.scss';
 
-export function SignInButton() {
-  const isUserLoggedIn = true;
+import { signIn, signOut, useSession } from 'next-auth/client';
 
-  return isUserLoggedIn ? (
+export function SignInButton() {
+  const [session] = useSession();
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04D361"></FaGithub>
-      Nilton Pontes
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417"></FaGithub>
       Sign in with Github
